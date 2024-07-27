7657f4c3d0f5804dc9db7750ece378ee932b1194
Vinicius Almeida <vinicius.almeida2011@gmail.com>
2024-07-27 17:21:00 -0300

    Criação projeto

---
+++ Produto.cs
@@ -0,0 +1,18 @@
+﻿namespace Course.Entities {
+    class Produto {
+
+        public string Nome { get; set; }
+        public double Preço { get; set; }
+        public int Quantidade { get; set; }
+
+        public Produto(string nome, double preço, int quantitade) {
+            Nome = nome;
+            Preço = preço;
+            Quantidade = quantitade;
+        }
+
+        public double Total() {
+            return Preço * Quantidade;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,42 @@
+﻿using Course.Entities;
+using System;
+using System.Globalization;
+using System.IO;
+
+namespace Course {
+    class Program {
+        static void Main(string[] args) {
+
+            Console.Write("Entre com pagina de busca:  ");
+            string sourceFilePath = Console.ReadLine();
+
+            try {
+                string[] Linha = File.ReadAllLines(sourceFilePath);
+
+                string sourceFolderPath = Path.GetDirectoryName(sourceFilePath);
+                string targetFolderPath = sourceFolderPath + @"\saida";
+                string targetFilePath = targetFolderPath + @"\sumario.csv";
+
+                Directory.CreateDirectory(targetFolderPath);
+
+                using (StreamWriter sw = File.AppendText(targetFilePath)) {
+                    foreach (string linha in Linha) {
+
+                        string[] fields = linha.Split(',');
+                        string nome = fields[0];
+                        double preço = double.Parse(fields[1], CultureInfo.InvariantCulture);
+                        int quantidade = int.Parse(fields[2]);
+
+                        Produto prod = new Produto(nome, preço, quantidade);
+
+                        sw.WriteLine(prod.Name + "," + prod.Total().ToString("F2", CultureInfo.InvariantCulture));
+                    }
+                }
+            }
+            catch (IOException e) {
+                Console.WriteLine("Erro Inesperado!");
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+}
